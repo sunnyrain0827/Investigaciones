c40fb9debf368d4f8d1993acdca19d48c088cf85
ruohoruotsi <ruoho.ruotsi@gmail.com>
2016-08-06 20:37:10 -0700

    [ADD] updates and rename to VAE code in Keras, adapted to take imdb text sequences, only to realize that I need LSTM instead of Dense layers ... launching a whole sequence variational auto-encoder line of inquiry

--- nnet/autoencoder_variational_sequence.py
+++ nnet/autoencoder_variational_sequence.py
@@ -7,11 +7,11 @@ Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6114
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras.layers import Input, Dense, Lambda
+from keras.layers import Input, Dense, Lambda, Embedding, LSTM
 from keras.models import Model
 from keras import backend as K
 from keras import objectives
-from keras.datasets import mnist
+from keras.datasets import mnist, reuters
 from keras.datasets import imdb
 from keras.preprocessing import sequence
 
@@ -20,7 +20,7 @@ original_dim = 784
 latent_dim = 2
 intermediate_dim = 128
 epsilon_std = 0.01
-nb_epoch = 100
+nb_epoch = 5
 
 x = Input(batch_shape=(batch_size, original_dim))
 h = Dense(intermediate_dim, activation='relu')(x)
@@ -29,8 +29,7 @@ z_log_std = Dense(latent_dim)(h)
 
 def sampling(args):
     z_mean, z_log_std = args
-    epsilon = K.random_normal(shape=(batch_size, latent_dim),
-                              mean=0., std=epsilon_std)
+    epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0., std=epsilon_std)
     return z_mean + K.exp(z_log_std) * epsilon
 
 # note that "output_shape" isn't necessary with the TensorFlow backend
@@ -51,38 +50,10 @@ def vae_loss(x, x_decoded_mean):
 vae = Model(x, x_decoded_mean)
 vae.compile(optimizer='rmsprop', loss=vae_loss)
 
-# train the VAE on MNIST digits
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-new_size_train = 20000
-new_size_test = 4000
-x_train = x_train[0:new_size_train].astype('float32') / 255.
-x_test = x_test[0:new_size_test].astype('float32') / 255.
-
-y_train = y_train[0:new_size_train]
-y_test = y_test[0:new_size_test]
-
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-
-print(x_train.shape, y_train.shape)
-print(x_test.shape, y_test.shape)
-
 # #########################################################################################################
-# # [IOHAVOC] attempts to repurpose the mnist VAE code to IMDB. It became quickly clear that
-# # we need to use a LSTM instead of just a Dense
-# (x_train_imdb, y_train_imdb), (x_test_imdb, y_test_imdb) = imdb.load_data(nb_words=5000, maxlen=None)
-#
-# # print('Pad sequences (samples x time)')
-# sentence_maxlen = 784
-# x_train_imdb = sequence.pad_sequences(x_train_imdb, maxlen=sentence_maxlen)
-# x_test_imdb  = sequence.pad_sequences(x_test_imdb, maxlen=sentence_maxlen)
-#
-# print(x_train_imdb.shape, y_train_imdb.shape)
-# print(x_test_imdb.shape, y_test_imdb.shape)
-#
-# x_train, y_train = x_train_imdb.astype('float32') / 1., y_train_imdb.astype('float32') / 1.
-# x_test, y_test = x_test_imdb.astype('float32') / 1., y_test_imdb.astype('float32') / 1.
+
+# # train the VAE on MNIST digits
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 #
 # new_size_train = 20000
 # new_size_test = 4000
@@ -92,9 +63,47 @@ print(x_test.shape, y_test.shape)
 # y_train = y_train[0:new_size_train]
 # y_test = y_test[0:new_size_test]
 #
-# # print(x_train.shape, y_train.shape)
-# # print(x_test.shape, y_test.shape)
+# x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+# x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 #
+# print(x_train.shape, y_train.shape)
+# print(x_test.shape, y_test.shape)
+
+# #########################################################################################################
+# # [IOHAVOC] attempts to repurpose the mnist VAE code to IMDB. It became quickly clear that
+# # we need to use a LSTM instead of just a Dense
+num_top_words = 8000.
+(x_train, y_train), (x_test, y_test) = imdb.load_data(nb_words=int(num_top_words), maxlen=None)
+
+# (x_train, y_train), (x_test, y_test) = reuters.load_data(path="reuters.pkl",
+#                                                          nb_words=None,
+#                                                          skip_top=0,
+#                                                          maxlen=None,
+#                                                          test_split=0.1)
+
+
+# word_index = reuters.get_word_index(path="reuters_word_index.pkl")
+
+sentence_maxlen = 784
+x_train = sequence.pad_sequences(x_train, maxlen=sentence_maxlen)
+x_test  = sequence.pad_sequences(x_test, maxlen=sentence_maxlen)
+
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+x_train, y_train = x_train.astype('float32') / num_top_words, y_train.astype('float32') / num_top_words
+x_test, y_test = x_test.astype('float32') / num_top_words, y_test.astype('float32') / num_top_words
+
+new_size_train = 20000
+new_size_test = 4000
+x_train = x_train[0:new_size_train]
+x_test = x_test[0:new_size_test]
+y_train = y_train[0:new_size_train]
+y_test = y_test[0:new_size_test]
+
+# print(x_train.shape, y_train.shape)
+# print(x_test.shape, y_test.shape)
+
 # #########################################################################################################
 
 vae.summary()
@@ -109,6 +118,7 @@ encoder = Model(x, z_mean)
 
 # display a 2D plot of the digit classes in the latent space
 x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+print(x_test_encoded.shape)
 plt.figure(figsize=(6, 6))
 plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
 plt.colorbar()
